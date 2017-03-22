304e359098e165b9a5d5366624014eaa3d970045
Saturnino Villaverde <savianco@hotmail.com>
2017-03-22 16:50:03 +0100

    advancing in project

--- client/src/app/components/shared/footer/footer.component.ts
+++ client/src/app/components/shared/footer/footer.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
 
